3574b3901e2ff9b4ee1e80ce0283c6d55e53bfcf
priya <priyaroy.sanoj@gmail.com>
2026-01-09 11:32:59 +0530

    Comment out ShopContextProvider implementation and related code

--- frontend/src/app/context/ShopContext.jsx
+++ frontend/src/app/context/ShopContext.jsx
@@ -1,41 +1,41 @@
-'use client';
-import { useState,createContext } from "react"
-import {products} from "../../assests/assets.js"
-export const ShopContext = createContext();
+// 'use client';
+// import { useState,createContext } from "react"
+// import {products} from "../../assests/assets.js"
+// export const ShopContext = createContext();
 
-const ShopContextProvider = (props) => {
-  const currency = "$";
-  const delivery_fee = 5;
-  const [search, setSearch] = useState("");
-  const [showSearch, setShowSearch] = useState(false);
-  const [cartItems,setCartItems]=useState({});
+// const ShopContextProvider = (props) => {
+//   const currency = "$";
+//   const delivery_fee = 5;
+//   const [search, setSearch] = useState("");
+//   const [showSearch, setShowSearch] = useState(false);
+//   const [cartItems,setCartItems]=useState({});
 
-  const addTocart = async (itemId,size)=> {
+//   const addTocart = async (itemId,size)=> {
 
-    let cartData = structuredClone (cartItems)
-    if (cartData[itemId]) {
-      if(cartData[itemId][size]){
-      if(cartData[itemId][size]+=)
-      }
+//     let cartData = structuredClone (cartItems)
+//     if (cartData[itemId]) {
+//       if(cartData[itemId][size]){
+//       if(cartData[itemId][size]+=)
+//       }
 
-    }
+//     }
 
-  }
-  const value = {
-    products,
-    currency,
-    delivery_fee,
-    search,
-    setSearch,
-    showSearch,
-    setShowSearch,
-  };
-// console.log("ShopContextProvider rendered with products:", products);
-  return (
-    <ShopContext.Provider value={value}>
-      {props.children}
-    </ShopContext.Provider>
-  );
-};
+//   }
+//   const value = {
+//     products,
+//     currency,
+//     delivery_fee,
+//     search,
+//     setSearch,
+//     showSearch,
+//     setShowSearch,
+//   };
 
-export default ShopContextProvider;
+//   return (
+//     <ShopContext.Provider value={value}>
+//       {props.children}
+//     </ShopContext.Provider>
+//   );
+// };
+
+// export default ShopContextProvider;
