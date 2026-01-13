1359ed02c3675586e42dda5d7e89257d2f42e86b
priya <priyaroy.sanoj@gmail.com>
2026-01-13 15:21:02 +0530

    Apply .gitignore rules

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,28 @@
+import express from "express";
+import upload from "../middleware/multer.js";
+import {
+  listProducts,
+  addProduct,
+  removeProduct,
+  singleProduct,
+} from "../controllers/productController.js";
+import adminAuth from "../middleware/adminAuth.js";
+
+const productRouter = express.Router();
+
+productRouter.post(
+  "/add",
+  adminAuth,
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
+productRouter.delete("/remove",adminAuth, removeProduct);
+productRouter.get("/single", singleProduct);
+productRouter.get("/list", listProducts);
+
+export default productRouter;
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import connectDB from "./config/db.js"
+import connectCloudinary from "./config/cloudinary.js"
+import userRoute from "./routes/userRoute.js"
+import productRouter from "./routes/productRoute.js"
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000 
+
+app.use(cors())    
+app.use(express.json()) 
+
+connectDB()   
+connectCloudinary() 
+
+//middleware 
+app.use(express.json());
+app.use(cors())
+
+// api endpoints
+app.use('/api/user',userRoute)
+app.use('/api/product',productRouter )
+
+app.get('/',(req,res)=> {
+    res.send ("API Working")
+})
+
+app.listen(port, ()=> console.log('server started on PORT :'+ port))
+  
